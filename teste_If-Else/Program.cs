e74e6d1354947a3ecdf7eb8a60d3a9424440c642
Romenildo Silva <Rnd_rn@hotmail.com>
2021-03-07 14:48:34 -0300

    aplicativo finalizado
    terminei

--- teste_If-Else/Program.cs
+++ teste_If-Else/Program.cs
@@ -11,18 +11,30 @@ namespace teste_If_Else
         static void Main(string[] args)
         {
             double gasolina = 0;
-            Console.Write("Digite consumo na gasolina: ");
+            Console.Write("Quanto Km faz com litro de gasolina: ");
             gasolina = Convert.ToDouble(Console.ReadLine());
 
             double alcool = 0;
-            Console.Write("Digite consumo no alcool: ");
+            Console.Write("Quanto Km faz com litro de alcool: ");
             alcool = Convert.ToDouble(Console.ReadLine());
 
             double quantidade = 0;
             Console.Write("Digite quantidade em litros para abastecer:");
             quantidade = Convert.ToDouble(Console.ReadLine());
 
+            double valorg = 0;
+            Console.Write("Digite valor do litro da gasolina: ");
+            valorg = Convert.ToDouble(Console.ReadLine());
+
+            double valora = 0;
+            Console.Write("Digite valor do litro do alcool: ");
+            valora = Convert.ToDouble(Console.ReadLine());
+
             if (gasolina * quantidade * 0.65 >= alcool * quantidade)
+            {
+                Console.WriteLine("Melhor abastecer gasolina");
+            }
+            else if ((valora * quantidade) / (valorg * quantidade) > 0.65)
             {
                 Console.WriteLine("Melhor abastecer gasolina");
             }
