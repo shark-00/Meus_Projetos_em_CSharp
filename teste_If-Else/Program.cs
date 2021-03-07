41b6aa0748aeb8f2ef2103a65ccf0246b63d7290
Romenildo Silva <Rnd_rn@hotmail.com>
2021-03-07 11:41:51 -0300

    Novo programa de consumo
    novo programa com if else

---
+++ teste_If-Else/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste_If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gasolina = 0;
+            Console.Write("Digite consumo na gasolina: ");
+            gasolina = Convert.ToDouble(Console.ReadLine());
+
+            double alcool = 0;
+            Console.Write("Digite consumo no alcool: ");
+            alcool = Convert.ToDouble(Console.ReadLine());
+
+            double quantidade = 0;
+            Console.Write("Digite quantidade em litros para abastecer:" +
+                "");
+            quantidade = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
