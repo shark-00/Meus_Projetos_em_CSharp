3567870785d7ccd34c4fd8a5a73179f15f643670
Romenildo Silva <Rnd_rn@hotmail.com>
2021-03-07 19:23:47 -0300

    trabalho _facul

---
+++ Trabalho_da_Facul/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_da_Facul
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hora de começar o trabalho da faculdade");
+
+
+            Console.ReadLine();
+        }
+    }
+}
