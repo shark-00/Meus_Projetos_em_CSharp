4f778eda4943797e5b8a0ccec9db97ef4462de61
Romenildo Silva <Rnd_rn@hotmail.com>
2021-03-06 21:07:40 -0300

    programa para abastecer
    com aumento do combustível qual esta mais  valendo a pena abastecer

---
+++ Combustivel/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaraçao de variavel
+            // Para ler double tem colocar (0;)
+            double alcool = 0;
+            // Lendo valor do alcool
+            Console.Write("Digite valor do alcool: ");
+            // Convertendo double em string para fazer a leitura
+            alcool = Convert.ToDouble(Console.ReadLine());
+
+            // Pulando linha
+            Console.WriteLine();
+            // Declaracao de variavel gasolina
+            double gasolina = 0;
+            Console.Write("Digite valor da gasolina: ");
+            gasolina = Convert.ToDouble(Console.ReadLine());
+
+            // Pulando linhas 
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Calculando qual mais favoravel usando if else
+            if (alcool / gasolina >= 0.7)
+            {
+                Console.WriteLine(" Melhor abaster na Gasolina");
+            }
+            else
+            {
+                Console.WriteLine(" Melhor abaster no Alcool");
+            }
+            // Pulando linha 
+            Console.WriteLine();
+            Console.WriteLine("Fim do Programa !");
+
+            
+
+            Console.ReadLine();
+           
+            
+
+        }
+    }
+}
