f86714a82f249e586eb7872e397c18bd2c4c756c
Romenildo Silva <Rnd_rn@hotmail.com>
2021-03-10 20:16:46 -0300

    novo_teste

---
+++ desvio_condicional/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desvio_condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // declaraçao de  variavel inteira sem valor 
+            int numero = 0;
+            Console.WriteLine("Escolha um número: 1, 2,ou 3");
+            Console.Write("Digite o número: ");
+            // convertendo variavel inteira para string para fazer a leitura do teclado 
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            // desvio condional switch, com a leitura da variavel inteira ativa um case diferente
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("Você digitou um");
+                    break;
+                case 2:
+                    Console.WriteLine(" Você digitou dois");
+                    break;
+                case 3:
+                    Console.WriteLine("Você digitou três");
+                    break;
+                default:
+                    Console.WriteLine("Número invalido");
+                    break;
+
+                        
+            }
+            /* laço de repetiçao for
+             * declaraçao de variavel inteira 0 
+             * laço irá conta do 0 ao 4*/
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Iteração {0}", i);
+            }
+
+            /* laço de repetiçao while
+             * declaraçao de variavel inteira 0
+             * laço ira conta do 0 ao 4*/
+            int ii = 0;
+            while (ii < 5);
+            {
+                Console.WriteLine("Iteração {0}", ii);
+                // diferente do FOR, while faz a contagem antes
+                ii++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ testando_switch_com_if_else/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testando_switch_com_if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {   // declaraçao de 3 variaves inteira 
+                int a, b, c = 0;
+                Console.WriteLine("Digite um número: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite outro número: ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Agora, o que você quer comesse números? ");
+                Console.WriteLine("1 - Verificar se o primeiro número é maior que o segundo número. ");
+                Console.WriteLine("2 - Verificar se o segundo número é maior que o primeiro número. ");
+                Console.WriteLine("3 - Verificar se os números são iguais. ");
+                c = Int32.Parse(Console.ReadLine());
+                switch (c)
+                {
+                    case 1:
+                        if (a > b)
+                        {
+                            Console.WriteLine("O primeiro número é maior que o segundo número! ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("O primeiro número não é maior que o segundo! ");
+                        }
+                        break;
+                    case 2:
+                        if (a < b)
+                        {
+                            Console.WriteLine("O segundo número é maior que o primeiro número! ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("O segundo número não é maior que o primeiro! ");
+                        }
+                        break;
+                    case 3:
+                        if (a == b)
+                        {
+                            Console.WriteLine("Os dois números são iguais! ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Os dois números não são iguais! ");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+                Console.WriteLine("Pressione qualquer tecla para sair...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
